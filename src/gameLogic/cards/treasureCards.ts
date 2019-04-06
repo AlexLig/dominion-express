6f971d838c6572f6f3f19e39288315fd71a088e6
AlexLig <ligkalex@gmail.com>
2019-04-07 01:56:11 +0300

    Update treasureCards.ts

--- src/gameLogic/cards/treasureCards.ts
+++ src/gameLogic/cards/treasureCards.ts
@@ -2,7 +2,7 @@ import { IGameState, ITurn } from '../interfaces/IGameState';
 import { addTreasure } from '../effects/addTreasure';
 
 const playTreasure = (amount: number) => (state: IGameState): IGameState => {
-  const turn: ITurn = addTreasure(state.turn, 3);
+  const turn: ITurn = addTreasure(state.turn, amount);
   return { ...state, turn };
 };
 export const playGold = playTreasure(3);

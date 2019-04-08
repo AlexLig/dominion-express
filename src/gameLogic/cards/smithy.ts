ef058dfe89a172fee3779f998befa867d50871c9
AlexLig <ligkalex@gmail.com>
2019-04-08 14:53:00 +0300

    Flatten reducer.

--- src/gameLogic/cards/smithy.ts
+++ src/gameLogic/cards/smithy.ts
@@ -1,8 +1,9 @@
 import { IGameState } from '../interfaces/IGameState';
 import { drawCards } from '../effects/drawCards';
 import { effectOnPlayerFromPlayers } from '../helpers/effectOnPlayerFromPlayers';
+import { IAction } from '../interfaces/IAction';
 
-export const playSmithy = (state: IGameState): IGameState => {
+export const playSmithy = (state: IGameState, action: IAction): IGameState => {
   const effectOnPlayer = effectOnPlayerFromPlayers(state.players);
   return { ...state, players: effectOnPlayer(drawCards(3)) };
 };
--- src/gameLogic/reducer.ts
+++ src/gameLogic/reducer.ts
@@ -1,11 +1,21 @@
 import { IGameState } from './interfaces/IGameState';
 import { IAction } from './interfaces/IAction';
-import { playCardReducer } from './playCardReducer';
+import { playGold, playSilver, playCopper } from './cards/treasureCards';
+import { playSmithy } from './cards/smithy';
 
 export const reducer = (state: IGameState, action: IAction) => {
   switch (action.type) {
-    case 'PLAY_CARD':
-      return playCardReducer(state, action);
+    case 'PLAY_GOLD':
+      return playGold(state, action);
+    case 'PLAY_SILVER':
+      return playSilver(state, action);
+
+    case 'PLAY_COPPER':
+      return playCopper(state, action);
+
+    case 'PLAY_SMITHY':
+      return playSmithy(state, action);
+
     default:
       return state;
   }

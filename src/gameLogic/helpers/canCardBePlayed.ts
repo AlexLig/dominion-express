3e39123eae60c27445866652e47344345ca6494b
AlexLig <ligkalex@gmail.com>
2019-04-08 14:52:35 +0300

    Throw error if active player is undefined.

--- src/gameLogic/helpers/canCardBePlayed.ts
+++ src/gameLogic/helpers/canCardBePlayed.ts
@@ -3,10 +3,12 @@ import { IAction } from '../interfaces/IAction';
 
 export const canCardBePlayed = (state: IGameState, action: IAction) => {
   const activePlayer = state.players.find(pl => pl.isHisTurn === true);
+  if (!activePlayer) throw new Error('players.find activePlayer not found!');
   const hasTheCard = activePlayer.hand.includes(action.cardName);
-  const isTheRightTurnPhase = state.turn.phase === action.cardPhase;
 
-  if (action.cardPhase === 'ACTION' && state.turn.actionPoints <= 0) return false;
+  const isTheRightTurnPhase = state.phase === action.cardPhase;
+
+  if (action.cardPhase === 'ACTION' && state.actionPoints <= 0) return false;
 
   return hasTheCard && isTheRightTurnPhase;
 };
--- src/gameLogic/helpers/effectOnPlayerFromPlayers.ts
+++ src/gameLogic/helpers/effectOnPlayerFromPlayers.ts
@@ -3,7 +3,7 @@ export const effectOnPlayerFromPlayers = (players: IPlayer[]) => (
   effect: (pl: IPlayer) => IPlayer,
 ): IPlayer[] => {
   const activePlayer = players.find(pl => pl.isHisTurn === true);
+  if (!activePlayer) throw new Error('players.find activePlayer not found!');
   const restPlayers = players.filter(pl => pl.isHisTurn === false);
-
-  return [...restPlayers, effect(activePlayer!)];
+  return [...restPlayers, effect(activePlayer)];
 };

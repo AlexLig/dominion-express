6ab2fdd55157533e161354cf246f521c1a4e0f1f
AlexLig <ligkalex@gmail.com>
2019-04-11 02:07:03 +0300

    remove smithy card from hand when played.

--- src/gameLogic/reducers/cards/smithy.ts
+++ src/gameLogic/reducers/cards/smithy.ts
@@ -2,10 +2,14 @@ import { IGameState } from '../../interfaces/IGameState';
 import { tryToDrawCards } from '../../effects/tryToDrawCards';
 import { IAction } from '../../interfaces/IAction';
 import { getApplyEffectsOnActive } from '../getApplyEffectsOnActive';
+import { moveFromHandToPlayed } from '../../effects/moveFromHandToPlayed';
 
 export const smithy = (state: IGameState, action: IAction): IGameState => {
   const effectOnPlayer = getApplyEffectsOnActive(state.players);
-  const players = effectOnPlayer(tryToDrawCards(3));
+  const players = effectOnPlayer(
+    tryToDrawCards(3),
+    moveFromHandToPlayed(action.cardName!),
+  );
   const actionPoints = state.actionPoints - 1;
   return { ...state, players, actionPoints };
 };

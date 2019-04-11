68f443dec9a9e32ff17602337c8580eabf8b0f2a
AlexLig <ligkalex@gmail.com>
2019-04-11 17:45:09 +0300

    cards cardList.

---
+++ src/cardList.ts
@@ -0,0 +1,10 @@
+import { CardList } from './gameLogic/types';
+import { copper, silver, gold } from './gameLogic/reducers/cardReducers/treasureCards';
+import { smithy } from './gameLogic/reducers/cardReducers/smithy';
+
+export const cards: CardList = {
+  COPPER: { name: 'COPPER', cost: 0, type: 'TREASURE', reducer: copper },
+  SILVER: { name: 'SILVER', cost: 3, type: 'TREASURE', reducer: silver },
+  GOLD: { name: 'GOLD', cost: 6, type: 'TREASURE', reducer: gold },
+  SMITHY: { name: 'SMITHY', cost: 4, type: 'ACTION', reducer: smithy },
+};
